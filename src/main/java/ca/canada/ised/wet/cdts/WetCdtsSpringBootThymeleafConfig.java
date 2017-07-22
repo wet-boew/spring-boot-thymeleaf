c94877d5005b9b51ea48d57df99d9baf2b93ecc4
Andrew Pitt <andrew.pitt@canada.ca>
2017-07-22 15:34:12 -0400

    Fixed multiple references to version 4_0_24 and leftnav js import.

--- src/main/java/ca/canada/ised/wet/cdts/WetCdtsSpringBootThymeleafConfig.java
+++ src/main/java/ca/canada/ised/wet/cdts/WetCdtsSpringBootThymeleafConfig.java
@@ -3,11 +3,11 @@
  */
 package ca.canada.ised.wet.cdts;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import ca.canada.ised.wet.cdts.components.wet.interceptor.WETTemplateInterceptor;
 
@@ -21,33 +21,34 @@ import ca.canada.ised.wet.cdts.components.wet.interceptor.WETTemplateInterceptor
 @ComponentScan(basePackages = "ca.canada.ised.wet.cdts.components")
 public class WetCdtsSpringBootThymeleafConfig {
 
-    /**
-     * Locale change interceptor.
-     *
-     * @return <code>LocaleChangeInterceptor</code>
-     */
-    @Bean(name = "localeChangeInterceptor")
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-        localeChangeInterceptor.setParamName("lang");
-        return localeChangeInterceptor;
-    }
+	// /**
+	// * Locale change interceptor.
+	// *
+	// * @return <code>LocaleChangeInterceptor</code>
+	// */
+	// @Bean(name = "localeChangeInterceptor")
+	// public LocaleChangeInterceptor localeChangeInterceptor() {
+	// LocaleChangeInterceptor localeChangeInterceptor = new
+	// LocaleChangeInterceptor();
+	// localeChangeInterceptor.setParamName("lang");
+	// return localeChangeInterceptor;
+	// }
 
-    /**
-     * WET Template Interceptor.
-     *
-     * @return WETTemplateInterceptor
-     */
-    @Bean(name = "wetTemplateInterceptor")
-    public WETTemplateInterceptor getTemplateHandler() {
-        return new WETTemplateInterceptor();
-    }
+	/**
+	 * WET Template Interceptor.
+	 *
+	 * @return WETTemplateInterceptor
+	 */
+	@Bean(name = "wetTemplateInterceptor")
+	public WETTemplateInterceptor getTemplateHandler() {
+		return new WETTemplateInterceptor();
+	}
 
-    @Bean(name = "applicationMessageSource")
-    public ResourceBundleMessageSource wetMessageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	@Bean(name = "applicationMessageSource")
+	public MessageSource wetMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 
-        messageSource.setBasenames("cdn/cdn", "cdn/messages");
-        return messageSource;
-    }
+		messageSource.setBasenames("cdn/cdn", "cdn/messages");
+		return messageSource;
+	}
 }
