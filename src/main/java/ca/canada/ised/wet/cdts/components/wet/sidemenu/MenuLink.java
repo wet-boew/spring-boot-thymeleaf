8c7682d82008cbc4a20c4809212e34160d5c0420
Andrew Pitt <Andrew.Pitt@ic.gc.ca>
2017-07-27 14:16:14 -0400

    Updated leftmenu-layout.

--- src/main/java/ca/canada/ised/wet/cdts/components/wet/sidemenu/MenuLink.java
+++ src/main/java/ca/canada/ised/wet/cdts/components/wet/sidemenu/MenuLink.java
@@ -14,98 +14,111 @@ import ca.canada.ised.wet.cdts.components.wet.utils.Language;
  */
 public class MenuLink implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/** The displayed menu text. */
-	private String text;
+    /** The displayed menu text. */
+    private String text;
 
-	/** The internal menu text en. */
-	private String textEn;
+    /** The internal menu text en. */
+    private String textEn;
 
-	/** The internal menu text fr. */
-	private String textFr;
+    /** The internal menu text fr. */
+    private String textFr;
 
-	/** The url for the menu. */
-	private String href;
+    /** The url for the menu. */
+    private String href;
 
-	/**
-	 * Gets the menu text.
-	 *
-	 * @return the text
-	 */
-	public String getText() {
-		if (Language.isEnglish()) {
-			return textEn;
-		} else {
-			return textFr;
-		}
-	}
+    /** Does the link open in a new window? */
+    private boolean newWindow;
 
-	/**
-	 * Sets the menu text.
-	 *
-	 * @param text
-	 *            the new text
-	 */
-	public void setText(String text) {
-		this.text = text;
-	}
+    /**
+     * Gets the menu text.
+     *
+     * @return the text
+     */
+    public String getText() {
+        if (Language.isEnglish()) {
+            return textEn;
+        } else {
+            return textFr;
+        }
+    }
 
-	/**
-	 * Gets the href.
-	 *
-	 * @return the href
-	 */
-	public String getHref() {
-		return href;
-	}
+    /**
+     * Sets the menu text.
+     *
+     * @param text the new text
+     */
+    public void setText(String text) {
+        this.text = text;
+    }
 
-	/**
-	 * Sets the href.
-	 *
-	 * @param href
-	 *            the new href
-	 */
-	public void setHref(String href) {
-		this.href = href;
-	}
+    /**
+     * Gets the href.
+     *
+     * @return the href
+     */
+    public String getHref() {
+        return href;
+    }
 
-	/**
-	 * Gets the text en.
-	 *
-	 * @return the text en
-	 */
-	public String getTextEn() {
-		return textEn;
-	}
+    /**
+     * Sets the href.
+     *
+     * @param href the new href
+     */
+    public void setHref(String href) {
+        this.href = href;
+    }
 
-	/**
-	 * Sets the text en.
-	 *
-	 * @param textEn
-	 *            the new text en
-	 */
-	public void setTextEn(String textEn) {
-		this.textEn = textEn;
-	}
+    /**
+     * Gets the text en.
+     *
+     * @return the text en
+     */
+    public String getTextEn() {
+        return textEn;
+    }
 
-	/**
-	 * Gets the text fr.
-	 *
-	 * @return the text fr
-	 */
-	public String getTextFr() {
-		return textFr;
-	}
+    /**
+     * Sets the text en.
+     *
+     * @param textEn the new text en
+     */
+    public void setTextEn(String textEn) {
+        this.textEn = textEn;
+    }
 
-	/**
-	 * Sets the text fr.
-	 *
-	 * @param textFr
-	 *            the new text fr
-	 */
-	public void setTextFr(String textFr) {
-		this.textFr = textFr;
-	}
+    /**
+     * Gets the text fr.
+     *
+     * @return the text fr
+     */
+    public String getTextFr() {
+        return textFr;
+    }
+
+    /**
+     * Sets the text fr.
+     *
+     * @param textFr the new text fr
+     */
+    public void setTextFr(String textFr) {
+        this.textFr = textFr;
+    }
+
+    /**
+     * @return the newWindow
+     */
+    public boolean isNewWindow() {
+        return newWindow;
+    }
+
+    /**
+     * @param newWindow the newWindow to set
+     */
+    public void setNewWindow(boolean newWindow) {
+        this.newWindow = newWindow;
+    }
 
 }
