482a1f37a33b16443683f19f65948b881b64f42e
Jim Sellers <jim.sellers@gmail.com>
2023-11-18 20:20:04 -0500

    adding more coverage to the smoke tests

--- src/test/java/ca/canada/ised/wet/cdts/components/wet/interceptor/WETTemplateInterceptorTest.java
+++ src/test/java/ca/canada/ised/wet/cdts/components/wet/interceptor/WETTemplateInterceptorTest.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 import ca.canada.ised.wet.cdts.components.wet.breadcrumbs.AbstractMockMvcTest;
 import ca.canada.ised.wet.cdts.components.wet.config.WETModelKey;
 import ca.canada.ised.wet.cdts.components.wet.config.WETSettings;
+import ca.canada.ised.wet.cdts.components.wet.config.beans.WETUrl;
 
 @DirtiesContext
 public class WETTemplateInterceptorTest extends AbstractMockMvcTest {
@@ -71,12 +72,22 @@ public class WETTemplateInterceptorTest extends AbstractMockMvcTest {
         wetSettings.getSession().setRefreshonclick(null);
         wetSettings.getSession().getRefreshlimit().setValue(null);
 
+        // footer stuff
+        wetSettings.getContact().getEnglish().setUrl("https://example.com/content-eng");
+        wetSettings.getPrivacy().getEnglish().setUrl("https://example.com/privacy-eng");
+        wetSettings.getTerms().getEnglish().setUrl("https://example.com/terms-eng");
+
         ModelAndView mav = new ModelAndView("unit-test");
         interceptor.postHandle(request, response, null, mav);
         // TODO check that the model objects where put in here correctly
 
         assertNotNull(mav.getModel().get(WETModelKey.SESSION_TIMEOUT.wetAttributeName()),
             "session timeout shouldn't be null");
+
+        // footer
+        assertNotNull(mav.getModel().get(WETModelKey.CONTACT_LINK.wetAttributeName()), "contact url shouldn't be null");
+        assertNotNull(mav.getModel().get(WETModelKey.PRIVACY_LINK.wetAttributeName()), "privacy shouldn't be null");
+        assertNotNull(mav.getModel().get(WETModelKey.TERMS_LINK.wetAttributeName()), "terms shouldn't be null");
     }
 
 }
